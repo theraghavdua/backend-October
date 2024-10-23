679bd206d794f88a24e1a3e0ef99c31bf3842bf5
theraghavdua <raghavdua130@gmail.com>
2024-10-23 15:47:35 +0530

    integrated hbs+partials

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
+const hbs = require("hbs");
+const path = require("path");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -27,7 +29,28 @@ app.get('/',(req,res)=>{
     res.send("working");
 });
 
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
 
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // APP CONFIG START
 app.listen(port, () =>{
